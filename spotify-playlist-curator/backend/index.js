bb44196653231d748ad03fd8db55f95180d81240
jarooz <soorajcodez@gmail.com>
2024-08-12 12:47:02 -0400

    added express.js

--- spotify-playlist-curator/backend/index.js
+++ spotify-playlist-curator/backend/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from '../src/App';
+import reportWebVitals from '../src/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +10,18 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from the backend!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
